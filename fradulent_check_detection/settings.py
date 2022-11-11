faf645baa6e10fb24258b6a35e83c51bd1cf59c6
Yasmine <73404550+Yasmineoueslati@users.noreply.github.com>
2022-11-11 09:17:19 +0100

    Update settings.py

--- fradulent_check_detection/settings.py
+++ fradulent_check_detection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nn12a=_ions)cp3)xrvbwvj4@o_q5anwt7m*^b@#$t9yyom5d('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
